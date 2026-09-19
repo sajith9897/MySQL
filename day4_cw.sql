7841a5536f7aa7caea915a217662e81b01c533d0
Sajith <sajith2005m@gmail.com>
2026-09-19 19:56:11 +0530

    Add SQL script for managing book records including insert, update, and delete operations

---
+++ day4_cw.sql
@@ -0,0 +1,17 @@
+INSERT INTO books (book_id, title, author, price, stock)
+VALUES (1, 'Learn SQL', 'John Smith', 400, 10),(2, 'Mastering Python', 'Jane Doe', 600, 5), 
+(3, 'HTML & CSS Basics', 'Alan Webb', 300, 8);
+
+-- Increase the price of the book 'Learn SQL' by 50 and update its stock to 12.
+UPDATE books
+SET price = 50, stock = 10;
+WHERE title = 'Learn SQL'
+
+-- Decrease the stock by 2 for books where the price is greater than 500.
+UPDATE books
+SET stock = 2;
+WHERE price > 500
+
+-- Delete the book that has book_id = 3.
+DELETE FROM books
+WHERE book_id = 3
