6250380b98a00194d406c8e81a4a94e458bf40e6
Sajith <sajith2005m@gmail.com>
2026-09-19 20:16:35 +0530

    Fix SQL script for updating book prices and stock adjustments

--- day4_cw.sql
+++ day4_cw.sql
@@ -4,12 +4,12 @@ VALUES (1, 'Learn SQL', 'John Smith', 400, 10),(2, 'Mastering Python', 'Jane Doe
 
 -- Increase the price of the book 'Learn SQL' by 50 and update its stock to 12.
 UPDATE books
-SET price = 50, stock = 10;
+SET price = price + 50, stock = 12
 WHERE title = 'Learn SQL'
 
 -- Decrease the stock by 2 for books where the price is greater than 500.
 UPDATE books
-SET stock = 2;
+SET stock = stock - 2
 WHERE price > 500
 
 -- Delete the book that has book_id = 3.
